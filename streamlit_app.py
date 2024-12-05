75dcd74a78bb93b344102c2a1df3665d8804b90a
Gareth Hampson <84174607+GarethHampsonMC@users.noreply.github.com>
2024-12-05 13:33:28 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 time_to_insert = ''
+smoothie_froot_response = ''
 
 ingredients_list = st.multiselect (
     'Choose up to 5 ingredients:'
