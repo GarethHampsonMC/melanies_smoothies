f43db2081f8041c74a8572f122da35a59e8b8911
Gareth Hampson <84174607+GarethHampsonMC@users.noreply.github.com>
2024-12-05 13:35:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 time_to_insert = ''
-smoothie_froot_response = ''
+smoothiefroot_response = ''
 
 ingredients_list = st.multiselect (
     'Choose up to 5 ingredients:'
@@ -65,7 +65,7 @@ if time_to_insert:
 
     st.success('Your Smoothie is ordered! ' + name_on_order, icon = "✅")
 
-    smoothie_froot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     st.text(smoothiefroot_response)
 
 
